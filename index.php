e416a18430a3192d67e4c845183d8c9294e611e6
ChakoRus <chalenkoa@yandex.ru>
2025-04-23 20:45:31 +0300

    fix browser cache reset

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/assets/css/b_player_drive.css">
+    <link rel="stylesheet" href="/assets/css/b_player_drive.css?v=<?= filemtime(__DIR__.'/assets/css/b_player_drive.css') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -144,7 +144,7 @@
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-<script src="/assets/js/slider.js"></script>
+<script src="/assets/js/slider.js?v=<?= filemtime(__DIR__.'/assets/js/slider.js') ?>"></script>
 <script>
     $(function() {
         let slider = Slider.create()
